6d069fb8dd5f500be9c0f7e9d6210239e77210f6
luxin1 <luxin1@corp.netease.com>
2019-03-19 17:44:11 +0800

    feat(all): cluster thread manage

---
+++ README.md
@@ -0,0 +1,10 @@
+## Thread-Mana
+
+* Mana(Devil Hunter: Mana Burn)
+* Mana(Manager)
+
+### Foreword
+
+A _Thread pool_ for Node.js *immediate Tasks*(We will **NOT** consider the Node version which lower than 11.0.0).
+
+
--- app/test.js
+++ app/test.js
@@ -1,18 +1,8 @@
-const Cluster = require('../lib/index');
+const Cluster = require('../lib/index1');
 const path = require('path');
 
 const cluster = Cluster.initCluster({
-    tasks: {
-        app: {
-            file: './app.js',
-            core: 4,
-            watch: true,
-        },
-        agent: {
-            file: './agent.js',
-            core: 3,
-            watch: false,
-        }
-    },
-    maxThreads: 4,
+    maxThreads: 4
 });
+
+cluster.close();
