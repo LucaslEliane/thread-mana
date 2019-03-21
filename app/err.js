e6462c9f2674190d1509a7da80443a7bebbe610c
luxin1 <luxin1@corp.netease.com>
2019-03-21 15:43:23 +0800

    feat(worker): add worker thread wrapper

---
+++ app/err.js
@@ -0,0 +1,3 @@
+console.log('app');
+
+throw new Error('this is app error');
--- app/test.js
+++ app/test.js
@@ -1,8 +1,25 @@
-const Cluster = require('../lib/index1');
+const Cluster = require('../lib/index');
 const path = require('path');
 
 const cluster = Cluster.initCluster({
-    maxThreads: 4
+    maxThreads: 4,
+    logDir: path.resolve(__dirname, 'log'),
+    inspectionCycle: 500,
+    env: 'prod',
 });
 
-cluster.close();
+cluster.setupWorker(
+    path.resolve(__dirname, './listen.js')
+);
+
+cluster.setupWorker(
+    path.resolve(__dirname, './normal.js')
+)
+
+cluster.setupWorker(
+    path.resolve(__dirname, './err.js')
+)
+
+setTimeout(() => {
+    cluster.close()
+}, 2000);
